0b8a9e0dcb4953709db8e7b98e44adf08c2bb193
cjlasley <cjlasley@alaska.edu>
2018-02-04 16:54:42 -0900

    Initial Commit: basic Hello World demo

---
+++ main.c
@@ -0,0 +1,17 @@
+/*-----------------------------------------------------------------
+ | CS 998: Technically Legal C Abuse               			      |
+ | Assignment 1: Optimized "Hello, World!"          		      |
+ | Due Date: 2/7/25                              		          |
+ | File: main.c                                 		          |
+ | Written By: Collin Lasley and Hart Chrisman     	 		      |
+ | Description: The fastest and hardest to read Demo Code around! |
+ -----------------------------------------------------------------*/
+ 
+ #include <stdio.h>
+ 
+ int main(void)
+ {
+ 	printf("Hello, World!\n");
+ 	
+ 	return 0;
+ }
